3e93c5aaeb02ae2114c8db33f962e20ec6ee2663
Francis Reed <francis.reed@gmail.com>
2022-07-04 23:50:36 -0400

    fix

--- apps/sender/app.py
+++ apps/sender/app.py
@@ -19,7 +19,7 @@ sender = create_sender(connect_to=connect_to)
 time_between_restarts = 5
 
 rpi_name = socket.gethostname() # send unique RPi hostname with each image
-picam = VideoStream(usePiCamera=True, resolution=(320, 240)).start()
+picam = VideoStream(usePiCamera=True, resolution=(240, 180)).start()
 time.sleep(2.0)  # allow camera sensor to warm up
 
 fps = FPS().start()
