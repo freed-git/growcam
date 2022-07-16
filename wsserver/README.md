4780b5bc2b8e7a5f6b98d4392943e24149ef0e96
Francis Reed <francis.reed@gmail.com>
2022-07-15 22:58:31 -0400

    fixed async sleep

--- wsserver/README.md
+++ wsserver/README.md
@@ -7,6 +7,11 @@ kubectl rollout restart deploy wsserver
 
 kubectl port-forward deployment/wsserver 32080:80
 
+kubectl scale deployment/wsserver --replicas=10
+
 nc -zv localhost 32080
 python -m websockets ws://localhost:32080/
 time python benchmark.py 500 6
+
+
+curl http://localhost:32080/inemuri
--- wsserver/app.py
+++ wsserver/app.py
@@ -13,7 +13,8 @@ async def slow_echo(websocket):
     async for message in websocket:
         # Block the event loop! This allows saturating a single asyncio
         # process without opening an impractical number of connections.
-        time.sleep(0.1)  # 100ms
+        await asyncio.sleep(0.1)
+        # time.sleep(0.1)  # 100ms
         await websocket.send(message)
 
 
@@ -21,8 +22,9 @@ async def health_check(path, request_headers):
     if path == "/healthz":
         return http.HTTPStatus.OK, [], b"OK\n"
     if path == "/inemuri":
-        loop = asyncio.get_running_loop()
-        loop.call_later(1, time.sleep, 10)
+        # loop = asyncio.get_running_loop()
+        # loop.call_later(1, asyncio.create_task, asyncio.sleep(10))
+        await asyncio.sleep(10)
         return http.HTTPStatus.OK, [], b"Sleeping for 10s\n"
     if path == "/seppuku":
         loop = asyncio.get_running_loop()
@@ -34,7 +36,7 @@ async def main():
     # Set the stop condition when receiving SIGTERM.
     loop = asyncio.get_running_loop()
     stop = loop.create_future()
-    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+    # loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
 
     async with websockets.serve(
         slow_echo,
